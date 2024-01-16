f0cd57a4330707db49bbaf69c72bcb4ed3250045
8harrison <monteiro4100@gmail.com>
2024-01-16 19:03:10 -0300

    Implementação do record PersonDto e do método toDto

--- src/main/java/com/betrybe/agrix/ebytr/staff/controllers/dto/PersonDto.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controllers/dto/PersonDto.java
@@ -1,5 +1,20 @@
 package com.betrybe.agrix.ebytr.staff.controllers.dto;
 
-public record PersonDto() {
+import com.betrybe.agrix.ebytr.staff.entity.Person;
 
+/**
+ * Implementação do record PersonDto.
+ * **/
+public record PersonDto(Long id, String username, String role) {
+
+  /**
+   * Implementação do método toDto.
+   * **/
+  public static PersonDto toDto(Person person) {
+    return new PersonDto(
+        person.getId(),
+        person.getUsername(),
+        person.getRole().name()
+    );
+  }
 }
