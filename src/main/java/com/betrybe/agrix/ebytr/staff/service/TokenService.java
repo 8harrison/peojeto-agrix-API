31f0c4638e8a19f746a466ffe9a23a2f19db82e4
8harrison <monteiro4100@gmail.com>
2024-01-17 09:51:57 -0300

    Implementação do método secret

--- src/main/java/com/betrybe/agrix/ebytr/staff/service/TokenService.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/service/TokenService.java
@@ -1,52 +1,60 @@
-//package com.betrybe.agrix.ebytr.staff.service;
-//
-//import com.auth0.jwt.JWT;
-//import com.auth0.jwt.algorithms.Algorithm;
-//import com.betrybe.agrix.ebytr.staff.entity.Person;
-//import java.time.Instant;
-//import java.time.LocalDateTime;
-//import java.time.ZoneOffset;
-//import java.util.Date;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Service;
-//
-///**
-// * Implementação da classe TokenService.
-// * **/
-//@Service
-//public class TokenService {
-//
-//  private final Algorithm algorithm;
-//
-//  public TokenService(@Value("${api.security.token.secret}") String secret) {
-//    this.algorithm = Algorithm.HMAC256(secret);
-//  }
-//
-//  /**
-//   * Implementação do método generateToken.
-//   * **/
-//  public String generateToken(Person person) {
-//    return JWT.create()
-//        .withIssuer("agrix")
-//        .withSubject(person.getUsername())
-//        .withExpiresAt(generateExpirationDate())
-//        .sign(algorithm);
-//  }
-//
-//  private Instant generateExpirationDate() {
-//    return LocalDateTime.now()
-//        .plusHours(2)
-//        .toInstant(ZoneOffset.of("-03:00"));
-//  }
-//
-//  /**
-//   * Implementação do método validateToken.
-//   * **/
-//  public String validateToken(String token) {
-//    return JWT.require(algorithm)
-//        .withIssuer("agrix")
-//        .build()
-//        .verify(token)
-//        .getSubject();
-//  }
-//}
+package com.betrybe.agrix.ebytr.staff.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementação da classe TokenService.
+ **/
+@Service
+public class TokenService {
+
+
+  private final Algorithm algorithm;
+
+  public TokenService() {
+    this.algorithm = Algorithm.HMAC256(secret());
+  }
+
+
+  /**
+   * Implementação do método generateToken.
+   **/
+  public String generateToken(Person person) {
+    return JWT.create()
+        .withIssuer("agrix")
+        .withSubject(person.getUsername())
+        .withExpiresAt(generateExpirationDate())
+        .sign(algorithm);
+  }
+
+  private Instant generateExpirationDate() {
+    return LocalDateTime.now()
+        .plusHours(2)
+        .toInstant(ZoneOffset.of("-03:00"));
+  }
+
+  /**
+   * Implementação do método validateToken.
+   **/
+  public String validateToken(String token) {
+    return JWT.require(algorithm)
+        .withIssuer("agrix")
+        .build()
+        .verify(token)
+        .getSubject();
+  }
+
+  private String secret() {
+    StringBuilder secret = new StringBuilder();
+    for (int i = 0; i < 11; i++) {
+      secret.append(Math.random());
+    }
+    return secret.toString();
+  }
+}
