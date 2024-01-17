cf03dbb1c72b98375e7e5bb05e6b7d4c60d413bf
8harrison <monteiro4100@gmail.com>
2024-01-17 09:50:08 -0300

    Implementação do método login

--- src/main/java/com/betrybe/agrix/ebytr/staff/controllers/AuthenticationController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controllers/AuthenticationController.java
@@ -1,42 +1,61 @@
-//package com.betrybe.agrix.ebytr.staff.controllers;
-//
-//import com.betrybe.agrix.ebytr.staff.controllers.dto.PersonDto;
-//import com.betrybe.agrix.ebytr.staff.entity.Person;
-//import com.betrybe.agrix.ebytr.staff.service.PersonService;
-//import com.betrybe.agrix.ebytr.staff.service.TokenService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * Implementação da classe AuthenticationController.
-// **/
-//@RestController
-//@RequestMapping("/auth")
-//public class AuthenticationController {
-//
-//  private final AuthenticationManager authenticationManager;
-//
-//  private final PersonService personService;
-//
-//  private final TokenService tokenService;
-//
-//  /**
-//   * Implementação do método construtor.
-//   **/
-//  @Autowired
-//  public AuthenticationController(AuthenticationManager authenticationManager,
-//      PersonService personService, TokenService tokenService) {
-//    this.authenticationManager = authenticationManager;
-//    this.personService = personService;
-//    this.tokenService = tokenService;
-//  }
-//
-//
-//}
+package com.betrybe.agrix.ebytr.staff.controllers;
+
+import com.betrybe.agrix.ebytr.staff.controllers.dto.AuthenticationDto;
+import com.betrybe.agrix.ebytr.staff.controllers.dto.PersonDto;
+import com.betrybe.agrix.ebytr.staff.controllers.dto.TokenDto;
+import com.betrybe.agrix.ebytr.staff.entity.Person;
+import com.betrybe.agrix.ebytr.staff.service.PersonService;
+import com.betrybe.agrix.ebytr.staff.service.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Implementação da classe AuthenticationController.
+ **/
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+  private final AuthenticationManager authenticationManager;
+
+  private final TokenService tokenService;
+
+  /**
+   * Implementação do método construtor.
+   **/
+  @Autowired
+  public AuthenticationController(AuthenticationManager authenticationManager,
+      TokenService tokenService) {
+    this.authenticationManager = authenticationManager;
+    this.tokenService = tokenService;
+  }
+
+  /**
+   * Implementação do método login.
+   * **/
+  @PostMapping("/login")
+  public ResponseEntity<TokenDto> login(@RequestBody AuthenticationDto authenticationDto) {
+    UsernamePasswordAuthenticationToken authentication =
+        new UsernamePasswordAuthenticationToken(
+            authenticationDto.username(), authenticationDto.password());
+    Authentication auth = authenticationManager.authenticate(authentication);
+
+    Person person = (Person) auth.getPrincipal();
+
+    String token = tokenService.generateToken(person);
+
+    TokenDto tokenDto = new TokenDto(token);
+
+    return ResponseEntity.ok(tokenDto);
+  }
+
+}
